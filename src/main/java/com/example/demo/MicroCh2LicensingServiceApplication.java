a69ffe0b350924722e0dbf5e3335f2b6755ce807
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-05 19:55:41 -0300

    init

---
+++ src/main/java/com/example/demo/MicroCh2LicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCh2LicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCh2LicensingServiceApplication.class, args);
+	}
+
+}
