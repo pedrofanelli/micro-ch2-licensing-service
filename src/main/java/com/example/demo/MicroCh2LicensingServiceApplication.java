acade1c4c19bda7b7f52f73637cd3821511a6fe9
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-05 19:59:55 -0300

    comment

--- src/main/java/com/example/demo/MicroCh2LicensingServiceApplication.java
+++ src/main/java/com/example/demo/MicroCh2LicensingServiceApplication.java
@@ -3,6 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * Core initialization logic for the microservice should be placed in this class.
+ * 
+ * @author peter
+ *
+ */
 @SpringBootApplication
 public class MicroCh2LicensingServiceApplication {
 
