a38b32f1b0d0eb8c30ddb1dda4f458a4b0ab4ce4
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-05 21:03:05 -0300

    more methods

--- src/main/java/com/example/demo/service/LicenseService.java
+++ src/main/java/com/example/demo/service/LicenseService.java
@@ -31,5 +31,20 @@ public class LicenseService {
 		return responseMessage;
 	}
 	
+	public String updateLicense(License license, String organizationId){ 
+		String responseMessage = null;
+		if (license != null) {
+	         license.setOrganizationId(organizationId);
+	         responseMessage = String.format("This is the put and the object is: %s", license.toString());
+		}
+	      return responseMessage;
+    }
+	
+	public String deleteLicense(String licenseId, String organizationId){ 
+		String responseMessage = null;
+		responseMessage = String.format("Deleting license with id %s for the organization %s",licenseId, organizationId);
+		return responseMessage;
+	}
+
 	
 }
