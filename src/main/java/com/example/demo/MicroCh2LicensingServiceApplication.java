51708b56e7138cb63ce3c841e72d48396451cac2
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-07 11:57:19 -0300

    actuator working

--- src/main/java/com/example/demo/MicroCh2LicensingServiceApplication.java
+++ src/main/java/com/example/demo/MicroCh2LicensingServiceApplication.java
@@ -16,6 +16,12 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
  * 
  * Core initialization logic for the microservice should be placed in this class.
  * 
+ * Los beans de mas abajo son para internacionalizacion, es decir, usar properties para cambiar texto devuelto en funcion de idioma
+ * 
+ * HATEOAS se usa para agregar links al objeto que se devuelve
+ * 
+ * Actuator se usa para obtener metricas y datos del estado del microservicio (data en application.properties)
+ * 
  * @author peter
  *
  */
