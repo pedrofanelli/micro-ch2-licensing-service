3552e09b71a1bba30353ae3c4e6099d52a7e14d5
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-07 10:20:59 -0300

    get path

--- src/main/java/com/example/demo/controller/LicenseController.java
+++ src/main/java/com/example/demo/controller/LicenseController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.License;
 import com.example.demo.service.LicenseService;
 
 @RestController
@@ -13,5 +17,15 @@ public class LicenseController {
 	@Autowired
 	private LicenseService licenseService;
 	
+	@GetMapping(value="/{licenseId}")
+	public ResponseEntity<License> getLicense (
+			@PathVariable("organizationId") String organizationId,
+			@PathVariable("licenseId") String licenseId) {
+		
+		License license = licenseService.getLicense(licenseId, organizationId);
+		
+		return ResponseEntity.ok(license);
+		
+	}
 	
 }
