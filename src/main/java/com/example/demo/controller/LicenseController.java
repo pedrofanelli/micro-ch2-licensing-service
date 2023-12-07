2f80b2c16e06f57c4e531142190cefff848b8a94
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-07 10:11:12 -0300

    building controller

--- src/main/java/com/example/demo/controller/LicenseController.java
+++ src/main/java/com/example/demo/controller/LicenseController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.LicenseService;
 
 @RestController
 @RequestMapping(value="v1/organization/{organizationId}/license")
 public class LicenseController {
 
+	@Autowired
+	private LicenseService licenseService;
+	
+	
 }
