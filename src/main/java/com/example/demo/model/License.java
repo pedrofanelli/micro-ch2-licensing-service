b08d55a2e1b4eb1381f1e0cc8d556c948918dd5d
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-07 11:16:34 -0300

    extendemos class en license model para hateoas

--- src/main/java/com/example/demo/model/License.java
+++ src/main/java/com/example/demo/model/License.java
@@ -1,11 +1,13 @@
 package com.example.demo.model;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @ToString
-public class License {
+public class License extends RepresentationModel<License> {
 
 	private int id;
 	private String licenseId;
