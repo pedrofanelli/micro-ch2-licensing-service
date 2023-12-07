a6a51b77db204091345a8d9de68d3973f90c862c
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-07 10:59:27 -0300

    messages properties

--- src/main/java/com/example/demo/MicroCh2LicensingServiceApplication.java
+++ src/main/java/com/example/demo/MicroCh2LicensingServiceApplication.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 
 /**
  * 
@@ -17,6 +24,21 @@ public class MicroCh2LicensingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroCh2LicensingServiceApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(true); // does not throw an error if a message is not found, instead it returns the message code
+		messageSource.setBasenames("messages"); // base name of the languages properties files
+		return messageSource;
 	}
 
 }
