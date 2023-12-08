0f98edc953a418cc5200f7bcfe16e5fcde88eb3f
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-08 12:59:22 -0300

    making license model to connect to db

--- src/main/java/com/example/demo/model/License.java
+++ src/main/java/com/example/demo/model/License.java
@@ -2,17 +2,39 @@ package com.example.demo.model;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @ToString
+@Entity
+@Table(name="licenses")
 public class License extends RepresentationModel<License> { //RepresentationModel<License> gives us the ability to 
 	                                                        //add links to the License model class. 
-	private int id;
+	@Id
+	@Column(name = "license_id", nullable = false)
 	private String licenseId;
+	
 	private String description;
+	
+	@Column(name = "organization_id", nullable = false)
 	private String organizationId;
+	
+	@Column(name = "product_name", nullable = false)
 	private String productName;
+	
+	@Column(name = "license_type", nullable = false)
 	private String licenseType;
+	
+	@Column(name="comment")
+	private String comment;
+	
+	public License withComment(String comment){
+		this.setComment(comment);
+		return this;
+	}
 }
