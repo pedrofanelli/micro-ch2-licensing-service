86cdb8e934f8090adc2093572d21e027a73139eb
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-08 13:14:57 -0300

    rebuilding service, controller

--- src/main/java/com/example/demo/controller/LicenseController.java
+++ src/main/java/com/example/demo/controller/LicenseController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,49 +26,34 @@ public class LicenseController {
 	private LicenseService licenseService;
 	
 	// endpoint: v1/organization/{organizationId}/license/{licenseId}
-	@GetMapping(value="/{licenseId}")
-	public ResponseEntity<License> getLicense (
-			@PathVariable("organizationId") String organizationId,
+	@GetMapping("/{licenseId}")
+	public ResponseEntity<License> getLicense( @PathVariable("organizationId") String organizationId,
 			@PathVariable("licenseId") String licenseId) {
 		
 		License license = licenseService.getLicense(licenseId, organizationId);
-		
-		// .add() is a method from the RepresentationModel added in the model License
-		// The linkTo method inspects the License controller class and obtains the root mapping, and the methodOn method obtains the method mapping by doing a dummy invocation of the target method.
-		// lo que hace es agregar los links a estos diferentes métodos para mostrar al cliente
 		license.add( 
 				linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
-				linkTo(methodOn(LicenseController.class).createLicense(organizationId, license, null)).withRel("createLicense"),
-				linkTo(methodOn(LicenseController.class).updateLicense(organizationId, license)).withRel("updateLicense"),
-				linkTo(methodOn(LicenseController.class).deleteLicense(organizationId, license.getLicenseId())).withRel("deleteLicense")
+				linkTo(methodOn(LicenseController.class).createLicense(license)).withRel("createLicense"),
+				linkTo(methodOn(LicenseController.class).updateLicense(license)).withRel("updateLicense"),
+				linkTo(methodOn(LicenseController.class).deleteLicense(license.getLicenseId())).withRel("deleteLicense")
 		);
 		
-		return ResponseEntity.ok(license); // ResponseEntity represents the entire HTTP response
-		
+		return ResponseEntity.ok(license);
 	}
-	
+
 	@PutMapping
-	public ResponseEntity<String> updateLicense(
-			@PathVariable("organizationId") String organizationId, 
-			@RequestBody License request) {
-		
-		return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+	public ResponseEntity<License> updateLicense(@RequestBody License request) {
+		return ResponseEntity.ok(licenseService.updateLicense(request));
 	}
 	
 	@PostMapping
-	public ResponseEntity<String> createLicense(
-			@PathVariable("organizationId") String organizationId, 
-			@RequestBody License request,
-			@RequestHeader(value = "Accept-Language",required = false) Locale locale) {
-		
-		return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
+	public ResponseEntity<License> createLicense(@RequestBody License request) {
+		return ResponseEntity.ok(licenseService.createLicense(request));
 	}
-	
+
 	@DeleteMapping(value="/{licenseId}")
-	public ResponseEntity<String> deleteLicense(
-			@PathVariable("organizationId") String organizationId, 
-			@PathVariable("licenseId") String licenseId) {
-		return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+	public ResponseEntity<String> deleteLicense(@PathVariable("licenseId") String licenseId) {
+		return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
 	}
 	
 }
--- src/main/java/com/example/demo/service/LicenseService.java
+++ src/main/java/com/example/demo/service/LicenseService.java
@@ -1,12 +1,13 @@
 package com.example.demo.service;
 
-import java.util.Locale;
-import java.util.Random;
+
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.config.ServiceConfig;
 import com.example.demo.model.License;
 import com.example.demo.repository.LicenseRepository;
 
@@ -18,42 +19,39 @@ public class LicenseService {
 	
 	@Autowired
 	private LicenseRepository licenseRepository;
-
-	public License getLicense(String licenseId, String organizationId) { 
-		
-		License license = new License();
-		license.setId(new Random().nextInt(1000)); 
-		license.setLicenseId(licenseId); 
-		license.setOrganizationId(organizationId); 
-		license.setDescription("Software product"); 
-		license.setProductName("Ostock"); 
-		license.setLicenseType("full");
-		return license;
-	   }
 	
-	public String createLicense(License license, String organizationId, Locale locale) { 
-		String responseMessage = null;
-		if(license != null) {
-			license.setOrganizationId(organizationId);
-			responseMessage = String.format(messages.getMessage("license.create.message",null,locale), license.toString());
-		}
+	@Autowired
+	ServiceConfig config;
 
-		return responseMessage;
+	public License getLicense(String licenseId, String organizationId){
+		License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+		if (null == license) {
+			throw new IllegalArgumentException(String.format(messages.getMessage("license.search.error.message", null, null),licenseId, organizationId));	
+		}
+		return license.withComment(config.getProperty());
 	}
-	
-	public String updateLicense(License license, String organizationId){ 
+
+	public License createLicense(License license){
+		license.setLicenseId(UUID.randomUUID().toString());
+		licenseRepository.save(license);
+
+		return license.withComment(config.getProperty());
+	}
+
+	public License updateLicense(License license){
+		licenseRepository.save(license);
+
+		return license.withComment(config.getProperty());
+	}
+
+	public String deleteLicense(String licenseId){
 		String responseMessage = null;
-		if (license != null) {
-	         license.setOrganizationId(organizationId);                                      // usa el Locale default (EN)
-	         responseMessage = String.format(messages.getMessage("license.update.message", null, null), license.toString());
-		}
-	      return responseMessage;
-    }
-	
-	public String deleteLicense(String licenseId, String organizationId){ 
-		String responseMessage = null;
-		responseMessage = String.format("Deleting license with id %s for the organization %s",licenseId, organizationId);
+		License license = new License();
+		license.setLicenseId(licenseId);
+		licenseRepository.delete(license);
+		responseMessage = String.format(messages.getMessage("license.delete.message", null, null),licenseId);
 		return responseMessage;
+
 	}
 
 	
