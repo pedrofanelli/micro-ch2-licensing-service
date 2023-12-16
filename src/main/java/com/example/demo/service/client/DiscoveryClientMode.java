544d901fd5e0d27460f9da6eac8a5a7ffd76a0b2
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-16 19:04:11 -0300

    DiscoveryClientMode set

--- src/main/java/com/example/demo/service/client/DiscoveryClientMode.java
+++ src/main/java/com/example/demo/service/client/DiscoveryClientMode.java
@@ -1,8 +1,15 @@
 package com.example.demo.service.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.model.Organization;
+import java.util.List;
 
 @Component
 public class DiscoveryClientMode {
