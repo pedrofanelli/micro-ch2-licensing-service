45daee2a3db1e8c20412463d7ebffb66780fd7d4
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-20 19:46:39 -0300

    beginning

--- src/main/java/com/example/demo/service/client/DiscoveryClientMode.java
+++ src/main/java/com/example/demo/service/client/DiscoveryClientMode.java
@@ -11,6 +11,11 @@ import org.springframework.web.client.RestTemplate;
 import com.example.demo.model.Organization;
 import java.util.List;
 
+/**
+ * The Spring Discovery Client offers the lowest level of access to the Load Balancer and the 
+ * services registered within it. Using the Discovery Client, you can query for all the services 
+ * registered with the Spring Cloud Load Balancer client and their corresponding URLs.
+ */
 @Component
 public class DiscoveryClientMode {
 
@@ -22,8 +27,10 @@ public class DiscoveryClientMode {
         List<ServiceInstance> instances = discoveryClient.getInstances("organization-service");
 
         if (instances.size()==0) return null;
+        // obtenemos el endpoint del servicio
         String serviceUri = String.format("%s/v1/organization/%s",instances.get(0).getUri().toString(), organizationId);
     
+        // restTemplate STANDARD para llamar al servicio
         ResponseEntity< Organization > restExchange =
                 restTemplate.exchange(
                         serviceUri,
