a8687ba3402eeaa5165893abdca15290f273790f
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-20 20:03:57 -0300

    comments

--- src/main/java/com/example/demo/service/client/DiscoveryClientMode.java
+++ src/main/java/com/example/demo/service/client/DiscoveryClientMode.java
@@ -15,6 +15,27 @@ import java.util.List;
  * The Spring Discovery Client offers the lowest level of access to the Load Balancer and the 
  * services registered within it. Using the Discovery Client, you can query for all the services 
  * registered with the Spring Cloud Load Balancer client and their corresponding URLs.
+ * 
+ * You use this class to interact with the Spring Cloud Load Balancer.
+ * 
+ * Then, to retrieve all instances of the organization services registered with Eureka, 
+ * you use the getInstances() method, passing in the service key that you’re looking for 
+ * to retrieve a list of ServiceInstance objects. The ServiceInstance class holds 
+ * information about a specific instance of a service, including its hostname, port, and URI.
+ * 
+ * There are some cons:
+ * 1) You aren’t taking advantage of the Spring Cloud client-side Load Balancer. By calling the 
+ * Discovery Client directly, you get a list of services, but it becomes your responsibility to 
+ * choose which returned service instance you’re going to invoke.
+ * 
+ * 2) You’re doing too much work. In the code, you have to build the URL that you’ll use to call 
+ * your service. It’s a small thing, but every piece of code that you can avoid writing is one 
+ * less piece of code that you have to debug.
+ * 
+ * 3) Observant Spring developers might have also noticed that we directly instantiated the 
+ * RestTemplate class in the code. This is antithetical to usual Spring REST invocations because 
+ * you’ll usually have the Spring framework inject the RestTemplate class via the @Autowired annotation.
+ * 
  */
 @Component
 public class DiscoveryClientMode {
