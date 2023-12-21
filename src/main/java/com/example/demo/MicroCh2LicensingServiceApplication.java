33c46f7abf050bb63a2d672d18d37db131caf973
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-21 19:28:50 -0300

    developing feign client

--- src/main/java/com/example/demo/MicroCh2LicensingServiceApplication.java
+++ src/main/java/com/example/demo/MicroCh2LicensingServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.client.RestTemplate;
@@ -37,6 +38,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 @SpringBootApplication
 @RefreshScope
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MicroCh2LicensingServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/service/client/FeignClientMode.java
@@ -0,0 +1,26 @@
+package com.example.demo.service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.model.Organization;
+
+/**
+ * The Feign library takes a different approach to call a REST service. With this approach, the 
+ * developer first defines a Java interface and then adds Spring Cloud annotations to map what 
+ * Eureka-based service the Spring Cloud Load Balancer will invoke. The Spring Cloud framework will 
+ * dynamically generate a proxy class to invoke the targeted REST service. There’s no code written 
+ * for calling the service other than an interface definition.
+ * 
+ * To enable the Feign client for use in our licensing service, we need to add a new 
+ * annotation, @EnableFeignClients in the main class.
+ */
+@FeignClient("organization-service") // identifies your service to Feign
+public interface FeignClientMode {
+
+	@GetMapping(
+            value="/v1/organization/{organizationId}",
+            consumes="application/json")
+    Organization getOrganization(@PathVariable("organizationId") String organizationId);
+}
