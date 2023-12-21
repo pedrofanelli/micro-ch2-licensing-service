5c35d95c7f91899566de1de402f83fb787d5afe7
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-21 19:35:17 -0300

    everything working... now comes Docker...

--- src/main/java/com/example/demo/service/client/FeignClientMode.java
+++ src/main/java/com/example/demo/service/client/FeignClientMode.java
@@ -15,6 +15,14 @@ import com.example.demo.model.Organization;
  * 
  * To enable the Feign client for use in our licensing service, we need to add a new 
  * annotation, @EnableFeignClients in the main class.
+ * 
+ * When you use the standard Spring RestTemplate class, all service call HTTP status codes are returned 
+ * via the ResponseEntity class’s getStatusCode() method. With the Feign client, any HTTP 4xx–5xx status 
+ * codes returned by the service being called are mapped to a FeignException. The FeignException 
+ * contains a JSON body that can be parsed for the specific error message. Feign provides the ability 
+ * to write an error decoder class that will map the error back to a custom Exception class. Writing 
+ * this decoder is outside the scope of this book, but you can find examples of this in the Feign 
+ * GitHub repository here: https://github.com/Netflix/feign/wiki/Custom-error-handling
  */
 @FeignClient("organization-service") // identifies your service to Feign
 public interface FeignClientMode {
