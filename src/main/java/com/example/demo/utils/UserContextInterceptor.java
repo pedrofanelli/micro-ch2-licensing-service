de241b9df21126d1d5d8b891f223b1f3a863da14
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-27 19:25:34 -0300

    User context interceptor

---
+++ src/main/java/com/example/demo/utils/UserContextInterceptor.java
@@ -0,0 +1,32 @@
+package com.example.demo.utils;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+/**
+ * Intercepta toda request http que se realice desde este microservicio, y le inyectamos los headers que necesitamos. En
+ * nuestro ejemplo, el Correlation ID.
+ */
+public class UserContextInterceptor implements ClientHttpRequestInterceptor {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserContextInterceptor.class);
+	
+    @Override
+    public ClientHttpResponse intercept(
+            HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+            throws IOException {
+
+        HttpHeaders headers = request.getHeaders();
+        headers.add(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
+        headers.add(UserContext.AUTH_TOKEN, UserContextHolder.getContext().getAuthToken());
+
+        return execution.execute(request, body);
+    }
+}
